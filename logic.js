cf9504ec8cefb40ad353566e5cfe5e9e793b850c
Marcus Hilaire <marcushilaire@gmail.com>
2018-07-17 13:34:56 -0400

    new structure

--- logic.js
+++ logic.js
@@ -1,18 +1,24 @@
-let network = {
-  cityA: [network.cityB, network.cityC, network.cityD],
-  cityB: ["cityA", "cityD"],
-  cityC: ["cityA", "cityD"],
-  cityD: ["cityA", "cityB", "cityC"]
-};
-// city a connects to b, c and d
-// city b connects to a and d
-// city c connects to a and d
-// city d connects to a, b and c
+// let network = {
+//   washington: ["seattle", "austin", "anaheim"],
+//   seattle: ["washington", "anaheim"],
+//   austin: ["washington", "anaheim"],
+//   anaheim: ["washington", "seattle", "austin"],
+//   test: this
+// };
+// // city a connects to b, c and d
+// // city b connects to a and d
+// // city c connects to a and d
+// // city d connects to a, b and c
 
-const oneJump = city => {
-  city.forEach((element, index) => {
-    console.log(element);
-  });
-};
-// console.log(network.cityA);
-// oneJump(network.cityA);
+// const oneJump = city => {
+//   city.forEach((element, index) => {
+//     console.log(element);
+//   });
+// };
+// console.log(network.test.bind(network));
+function City(name, portals) {
+  this.name = name;
+  this.portals = portals;
+}
+let washington = new City("washington", ["seattle", "austin", "anaheim"]);
+console.log(washington);
