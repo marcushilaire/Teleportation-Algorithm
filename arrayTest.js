161f497e8e3cc93d0ec9c8bc665f6235ec962da7
Marcus Hilaire <marcushilaire@gmail.com>
2018-07-19 10:22:19 -0400

    Will likely be using this array comparison method

--- arrayTest.js
+++ arrayTest.js
@@ -1,11 +1,24 @@
-const a = [9, 1];
+const a = [[9, 1], [8, 2], [4, 5]];
 const b = [1, 9];
+const c = [9, 1];
+const d = [5, 4];
 
 const arrCompare = (x, y) => {
-  // Expects two arrays(of length two) as arguments
-  if (y.includes(x[0]) && y.includes(x[1])) {
-    return true;
+  // x should be visited and y should be the new visit attempt
+  for (let i = 0; i < x.length; i++) {
+    if (x[i].includes(y[0]) && x[i].includes(y[1])) {
+      console.log("truthy");
+      return true;
+    }
+    console.log("falsy");
   }
-}; // returns true if the arrays contain the same values
+}; // returns true if the arrays contain essentially same values
 
-console.log(arrCompare(a, b));
+console.log(arrCompare(a, d));
+
+[1, 2, 3, 4, 5].forEach(element => {
+  if ((element = 4)) {
+    return;
+  }
+  console.log(element);
+});
