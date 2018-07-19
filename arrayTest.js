74e8fb3d444bebb0dc439b9bd036067b203e2e3e
Marcus Hilaire <marcushilaire@gmail.com>
2018-07-18 21:24:27 -0400

    array comparison tool

--- arrayTest.js
+++ arrayTest.js
@@ -1,10 +1,12 @@
 const a = [9, 1];
-
 const b = [1, 9];
+
 console.log(a.reverse());
 
-const arrCompare = (a, b) => {
-  if (a === b || a === b.reverse()) {
-    console.log("these are the same");
+const arrCompare = (x, y) => {
+  if (y.includes(x[0]) && y.includes(x[1])) {
+    console.log("essentially equal");
   }
 };
+
+arrCompare(a, b);
