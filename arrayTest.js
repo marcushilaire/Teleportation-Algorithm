6fd229192e12013cc417b85287660999b1c3b608
Marcus Hilaire <marcushilaire@gmail.com>
2018-07-18 21:26:16 -0400

    array comparison method

--- arrayTest.js
+++ arrayTest.js
@@ -1,12 +1,11 @@
 const a = [9, 1];
 const b = [1, 9];
 
-console.log(a.reverse());
-
 const arrCompare = (x, y) => {
+  // Expects two arrays(of length two) as arguments
   if (y.includes(x[0]) && y.includes(x[1])) {
-    console.log("essentially equal");
+    return true;
   }
-};
+}; // returns true if the arrays contain the same values
 
-arrCompare(a, b);
+console.log(arrCompare(a, b));
